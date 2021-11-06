8aaf38c4488348228e09477092aed56a298663cd
Tom <vandamme.tom@outlook.com>
2021-11-06 18:33:16 +0100

    Dynamic masthead

--- mechanix/website/cms_plugins.py
+++ mechanix/website/cms_plugins.py
@@ -1,6 +1,6 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from .models import MenuItem
+from .models import MenuItem, Masthead, MastheadButton, MastheadContent
 from django.utils.translation import gettext_lazy as _
 
 
@@ -10,6 +10,46 @@ class MenuItemPlugin(CMSPluginBase):
     name = _("Menu item")
     render_template = "menu-item.html"
     cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class MastheadPlugin(CMSPluginBase):
+    model = Masthead
+    name = _("Masthead")
+    render_template = "masthead.html"
+    cache = False
+    allow_children = True
+    child_classes = ['MastheadContentPlugin', 'MastheadButtonPlugin']
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class MastheadContentPlugin(CMSPluginBase):
+    model = MastheadContent
+    name = _("Masthead content")
+    render_template = "mastheadcontent.html"
+    cache = False
+    parent_classes = ['MastheadPlugin']
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class MastheadButtonPlugin(CMSPluginBase):
+    model = MastheadButton
+    name = _("Masthead button")
+    render_template = "mastheadbutton.html"
+    cache = False
+    parent_classes = ['MastheadPlugin']
 
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
--- mechanix/website/models.py
+++ mechanix/website/models.py
@@ -10,3 +10,24 @@ class MenuItem(CMSPlugin):
 
     def __str__(self):
         return self.displayName
+
+class Masthead(CMSPlugin):
+    background = models.ImageField(upload_to="Mechanix")
+    alt = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.alt
+
+class MastheadContent(CMSPlugin):
+    htmlClasses = models.CharField(max_length=256)
+    text = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.text
+
+class MastheadButton(CMSPlugin):
+    link = models.CharField(max_length=128)
+    text = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.text
