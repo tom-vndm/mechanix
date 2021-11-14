a19d81ab87aad79f7119db795710e852a6c844fe
Tom <vandamme.tom@outlook.com>
2021-11-14 17:52:51 +0100

    Added secret to private file

--- mechanix/mechanix/settings.py
+++ mechanix/mechanix/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
+from . import secret
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$c(+(_my4x+nu-xgnchpt&6%ya0@zsd7c=2p*)_*kt5rqd&4g8'
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
